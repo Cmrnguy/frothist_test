8cc792697bdaf26c4a26f816666e5e555c1c7036
camerondguy@gmail.com <camg@Cams-MacBook-Pro.local>
2014-09-18 12:29:00 -0700

    Initial Commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 FrothistTest::Application.config.secret_key_base = 'c237d7155a47db0594c376da2a759285b57d272f3601e07bbc45f6157ad577bfeb9049606d51bb3e8b1e3bab62b4d9ec54b72f5d52a1717e49b4c07c59b73d97'
