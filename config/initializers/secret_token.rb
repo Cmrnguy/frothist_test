5ed30e2d867471ebffb080f25395255fe57d214b
camerondguy@gmail.com <camg@Cams-MacBook-Pro.local>
2014-09-18 11:28:07 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FrothistTest::Application.config.secret_key_base = 'c237d7155a47db0594c376da2a759285b57d272f3601e07bbc45f6157ad577bfeb9049606d51bb3e8b1e3bab62b4d9ec54b72f5d52a1717e49b4c07c59b73d97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FrothistTest::Application.config.session_store :cookie_store, key: '_frothist_test_session'
